09fef859e34fae94ca47f2a0168b5371847b89eb
Alexander Serebrennikov <alex.n.serebrennikov@gmail.com>
2022-08-14 19:11:59 +0600

    added a Mod07

---
+++ Mod07/Exercise 3/Grades.DataModel/customGrade.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Grades.DataModel
+{
+    public partial class Grade
+    {
+        public bool ValidateAssessmentDate(DateTime date)
+        {
+            if (date > DateTime.Now)
+            {
+                throw new ArgumentOutOfRangeException("Date is later than the current day", "DateTime");
+            }
+
+            return true;
+        }
+
+        public bool ValidateAssessmentGrade(string assessment)
+        {
+            Match matchGrade = Regex.Match(assessment, @"^[A-E][+-]?$");
+
+            if (!matchGrade.Success)
+            {
+                throw new ArgumentOutOfRangeException("Assessment is not in the valid range", "Assessment");
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Mod07/Exercise 3/Grades.DataModel/customTeacher.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades.DataModel
+{
+    public partial class Teacher
+    {
+        private const int MAX_CLASS_SIZE = 8;
+
+        public void EnrollInClass(Student student)
+        {
+            int studentCount = (from s in Students
+                                        where s.TeacherUserId == UserId
+                                        select s).Count();
+
+            if (studentCount == MAX_CLASS_SIZE)
+            {
+                throw new ClassFullException("The class if full", "Student");
+            }
+
+            if (student.TeacherUserId != null)
+            {
+                throw new ArgumentException("Already assigned to a class", "Student");
+            } else
+            {
+                student.TeacherUserId = UserId;
+            }
+        }
+    }
+}
